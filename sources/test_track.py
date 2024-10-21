22ba8aab4e3aece39c8879681dd75e4ce0bd2c9a
biloute02 <fionadamien@gmail.com>
2024-10-21 18:24:40 +0200

    Test de tracking avec une video de 15s de foule en 360p 720p et 1080p. Les résolutions basses ne sont pas nécessaires car on peut changer la taille de l’image traitée. Fonction pour exporter au format NCNN.

--- sources/test_track.py
+++ sources/test_track.py
@@ -1,6 +1,21 @@
 from ultralytics import YOLO
+import cv2
 
 model = YOLO("yolo11n.pt")
 
+#video_path = "videos/tokyo_cats_festival_foule_1080p.webm"
+#cap = cv2.VideoCapture(video_path)
+#success, frame = cap.read()
+
 # Start tracking objects in a video
-model.track(source="url", show=True)
+model.track(
+    source = "videos/tokyo_cats_festival_foule_720p.webm",
+    #source=0,
+    #imgsz=(1080, 1920),
+    imgsz=(720, 1280),
+    #imgsz=(360, 640),
+    stream_buffer=True,
+    show=True,
+    conf=0.5,
+    classes=[0]
+)
